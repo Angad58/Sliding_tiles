da69258facc8886b37b18e5ee5187d4e26d54eeb
nula2720 <angadpalsingh.nul@uleth.ca>
2023-03-21 22:22:44 -0600

    Initial push

--- Board.h
+++ Board.h
@@ -1,1 +1,16 @@
-//fdf
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Board_Tile
+{
+public:
+    Board_Tile(const string &s);
+    vector<Board_Tile> nextConfigs();
+    int numMoves();
+    int Manhattan_Distance(const Board_Tile &goalconfig);
+
+    string config;
+    string movesFromStart;
+};
