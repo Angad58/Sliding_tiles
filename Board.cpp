6a4b126c16781973b3396d3c61aabc90c5e4138a
Angadpal singh <angadpalsingh.null@uleth.ca>
2023-03-28 02:22:14 -0600

    Manhattan distance function

--- Board.cpp
+++ Board.cpp
@@ -1,11 +1,37 @@
 #include "Board.h"
-
 Board_Tile::Board_Tile(const string &s) : config(s)
 {
 }
+vector<Board_Tile> Board_Tile::nextConfigs()
+{
+
+}
 int Board_Tile::Manhattan_Distance(const Board_Tile &goalconfig)
 {
-    return 0;
+   int distance = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        if (this->config[i] != '0')
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (this->config[i] == goalconfig.config[j])
+                {
+                distance += std::abs(i / 3 - j / 3) + std::abs(i % 3 - j % 3);
+                }
+            }
+        }
+    }
+    return distance;
+}
+
+string Board_Tile::move(int t,char m)
+{
+    if(m =='U')
+    if(m =='D')
+    if(m =='L')
+    if(m =='R')
+
 }
 
 string Board_Tile::getConfig()
--- Board.h
+++ Board.h
@@ -6,11 +6,24 @@ using namespace std;
 class Board_Tile
 {
 public:
+    /*Constructor, intializes the config of board object with the passed string */
     Board_Tile(const string &s);
+    /*Returns a vector of all possible next configurations of the board object after 1 move*/
     vector<Board_Tile> nextConfigs();
+
+    /*Returns the number of moves made from the inital configuration to the current configuration*/
     int numMoves();
+
+    /*Returns the Manhattan distance between the current configuration and the goal configuration*/
     int Manhattan_Distance(const Board_Tile &goalconfig);
+
+    /*Returns the configuration of the board object after the move m is made*/
+    string move(int t,char m);
+
+    /*Returns the configuration of the board object*/
     string getConfig();
+
+    /*Sets the configuration of the board object*/
     void setConfig(string s);
 
 private:
